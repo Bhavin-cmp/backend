349e46b5b962e108f120b11eba2292d96692082a
Bhavin <patelbc94@gmail.com>
2024-10-15 18:10:13 +0530

    fix refresh token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -197,7 +197,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookie.refreshToken || req.body.refreshToken;
 
-  if (incomingRefreshToken) {
+  if (!incomingRefreshToken) {
     throw new ApiError(401, "Unauthorized request");
   }
 
